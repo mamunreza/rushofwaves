94493cfa1b1a175c1da985f2dec7a2ec6e6cb3f8
Mamun Reza <mamun.reza09@hotmail.com>
2016-03-19 17:30:40 +0600

    Added generic types in single list

---
+++ basics/generics/BaseTypeUsage/BaseTypeUsage/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace BaseTypeUsage
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new List<Item>();
+
+            var ints = new Item<int>();
+            var doubles = new Item<double>();
+            var strings = new Item<string>();
+
+            items.Add(ints);
+            items.Add(doubles);
+            items.Add(strings);
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    public class Item
+    {
+
+    }
+
+    public class Item<T> : Item
+    {
+
+    }
+}
