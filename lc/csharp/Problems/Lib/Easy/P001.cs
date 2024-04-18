85da29bf5158da15b7c6dcaa39ac317a9251e8a2
Mamun Reza <mamun.reza09@gmail.com>
2024-04-18 11:03:07 +0300

    lc-1 P001 Two Sum (#1)
    Co-authored-by: Mamun Reza <Muhammad.Reza@energia.ee>

---
+++ lc/csharp/Problems/Lib/Easy/P001.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace Lib.Easy;
+
+public class P001
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Hashtable ht = new();
+        int[] result = new int[2];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var diff = target - nums[i];
+            if (ht.ContainsKey(diff))
+            {
+                result = [(int)ht[diff]!, i];
+                break;
+            }
+            ht[nums[i]] = i;
+        }
+        return result;
+    }
+}
---
+++ lc/csharp/Problems/Problems.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using Lib.Easy;
+
+namespace Problems.Cnsl;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        ExecuteTwoSum();
+    }
+
+    private static void ExecuteTwoSum()
+    {
+        int[] nums = { 2, 7, 11, 15 };
+        var p = new P001();
+        var result = p.TwoSum(nums, 9);
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
