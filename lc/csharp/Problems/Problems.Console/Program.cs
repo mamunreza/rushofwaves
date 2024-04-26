67f2922e018f89bbe089ee0d4edfcfdf73826279
Mamun Reza <mamun.reza09@gmail.com>
2024-04-26 10:31:17 +0300

    lc-3 (#4)
    Co-authored-by: Mamun Reza <Muhammad.Reza@energia.ee>

--- lc/csharp/Problems/Problems.Console/Program.cs
+++ lc/csharp/Problems/Problems.Console/Program.cs
@@ -4,6 +4,6 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        RunnerEasy.ExecuteP014();
+        RunnerMedium.ExecuteP005();
     }
 }
---
+++ lc/csharp/Problems/Problems.Console/RunnerMedium.cs
@@ -0,0 +1,26 @@
+﻿using Lib.Medium;
+
+namespace Problems.Cnsl;
+
+internal class RunnerMedium
+{
+
+    internal static void ExecuteP003()
+    {
+        //var s = "abcabcbb";
+        //var s = "bbbbb";
+        var s = "pwwkew";
+        //var s = "aabaab!bb";
+        var item = new P003();
+        var result = item.LengthOfLongestSubstring(s);
+        Console.WriteLine(result);
+    }
+
+    internal static void ExecuteP005()
+    {
+        var item = new P005();
+        var input = "aaba";
+        var result = item.LongestPalindrome(input);
+        Console.WriteLine(result);
+    }
+}
