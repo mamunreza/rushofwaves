4eb5a4870066ba544b94587ca39881868a805e17
Mamun Reza <Muhammad.Reza@energia.ee>
2024-04-26 10:30:51 +0300

    lc-3

---
+++ lc/csharp/Problems/Lib/Medium/P003.cs
@@ -0,0 +1,25 @@
+﻿namespace Lib.Medium;
+
+public class P003
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        int length = 0;
+        HashSet<char> subString = new HashSet<char>();
+        int l = 0;
+
+        for (int r = 0; r < s.Length; r++)
+        {
+            while (subString.Contains(s[r]))
+            {
+                subString.Remove(s[l]);
+                l += 1;
+            }
+
+            subString.Add(s[r]);
+            length = Math.Max(length, r - l + 1);
+        }
+
+        return length;
+    }
+}
---
+++ lc/csharp/Problems/Lib/Medium/P005.cs
@@ -0,0 +1,23 @@
+﻿namespace Lib.Medium;
+
+public class P005
+{
+    public string LongestPalindrome(string s)
+    {
+        for (int i = 0; i < s.Length; i++)
+        {
+            for (int l = i, r = i; l >= 0 && r < s.Length && s[l] == s[r]; l--, r++)
+            {
+                Console.WriteLine("l1 -> " + l + " - " + r);
+            }
+
+            for (int l = i, r = i + 1; l >= 0 && r < s.Length && s[l] == s[r]; l--, r++)
+            {
+                Console.WriteLine("l2 -> " + l + " - " + r);
+            }
+        }
+
+
+        return string.Empty;
+    }
+}
