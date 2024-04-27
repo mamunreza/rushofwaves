9372d208c9556ab57270cffeadae7e150b69c08c
Mamun Reza <mamun.reza09@gmail.com>
2024-04-28 01:12:52 +0300

    lc-11 (#5)
    Co-authored-by: Mamun Reza <Muhammad.Reza@energia.ee>

--- lc/csharp/Problems/Lib/Medium/P005.cs
+++ lc/csharp/Problems/Lib/Medium/P005.cs
@@ -4,20 +4,27 @@ public class P005
 {
     public string LongestPalindrome(string s)
     {
+        int resl = 0;
+        int resr = 0;
         for (int i = 0; i < s.Length; i++)
         {
-            for (int l = i, r = i; l >= 0 && r < s.Length && s[l] == s[r]; l--, r++)
+            for (int l = i, r = i + 1; l < s.Length; l++, r++)
             {
-                Console.WriteLine("l1 -> " + l + " - " + r);
-            }
-
-            for (int l = i, r = i + 1; l >= 0 && r < s.Length && s[l] == s[r]; l--, r++)
-            {
-                Console.WriteLine("l2 -> " + l + " - " + r);
+                while (l >= 0 && r < s.Length && s[l] == s[r])
+                {
+                    if (r - l > resr - resl)
+                    {
+                        resl = l;
+                        resr = r;
+                    }
+                    l--;
+                    r++;
+                    //Console.WriteLine("l: " + l + ", r: " + r);
+                }
             }
         }
 
 
-        return string.Empty;
+        return s.Substring(resl+1, resr+1);
     }
 }
