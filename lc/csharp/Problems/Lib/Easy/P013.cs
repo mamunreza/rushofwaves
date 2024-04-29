74086f08ac44708fda0e3ee9f515082a18937cb8
Mamun Reza <Muhammad.Reza@energia.ee>
2024-04-29 10:53:07 +0300

    lc-12 P013

---
+++ lc/csharp/Problems/Lib/Easy/P013.cs
@@ -0,0 +1,32 @@
+﻿namespace Lib.Easy;
+
+public class P013
+{
+    public int RomanToInt(string s)
+    {
+        Dictionary<char, int> mapping = new()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        int prev = 0;
+        int result = 0;
+        foreach(var c in s.Reverse())
+        {
+            if (mapping[c] < prev)
+                result -= mapping[c];
+            else
+                result += mapping[c];
+
+            prev = mapping[c];
+        }
+
+        return result;
+    }
+}
