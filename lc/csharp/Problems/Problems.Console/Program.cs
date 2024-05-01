408b73c1859d6bec4d04661dddbfbb84ad788cda
Mamun Reza <Muhammad.Reza@energia.ee>
2024-05-02 00:11:19 +0300

    lc-53

--- lc/csharp/Problems/Problems.Console/Program.cs
+++ lc/csharp/Problems/Problems.Console/Program.cs
@@ -4,6 +4,6 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        RunnerEasy.ExecuteP027();
+        RunnerMedium.ExecuteP053();
     }
 }
--- lc/csharp/Problems/Problems.Console/RunnerMedium.cs
+++ lc/csharp/Problems/Problems.Console/RunnerMedium.cs
@@ -23,12 +23,23 @@ internal class RunnerMedium
         var result = item.LongestPalindrome(input);
         Console.WriteLine(result);
     }
+
     internal static void ExecuteP011()
     {
         var item = new P011();
         //int[] input = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
         int[] input = new int[] { 1, 1 };
         var result = item.MaxArea(input);
+        Console.WriteLine(result);
+    }
+
+    internal static void ExecuteP053()
+    {
+        var item = new P053();
+        //int[] nums = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        //int[] nums = new int[] { 1 };
+        int[] nums = new int[] { 5, 4, -1, 7, 8 };
+        var result = item.MaxSubArray(nums);
         Console.WriteLine(result);
     }
 }
