ed2d995443aca305348dfca6b58cd3f583935f0b
Mamun Reza <mamun.reza09@gmail.com>
2024-05-02 00:11:47 +0300

    lc-53 (#9)
    Co-authored-by: Mamun Reza <Muhammad.Reza@energia.ee>

---
+++ lc/csharp/Problems/Lib/Medium/P053.cs
@@ -0,0 +1,20 @@
+﻿namespace Lib.Medium;
+
+public class P053
+{
+    public int MaxSubArray(int[] nums)
+    {
+        int maxSum = nums[0];
+        int currentsum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (currentsum < 0)
+                currentsum = 0;
+            currentsum += nums[i];
+            maxSum = Math.Max(maxSum, currentsum);
+        }
+
+        return maxSum;
+    }
+}
