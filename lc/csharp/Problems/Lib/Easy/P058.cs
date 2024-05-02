50634f9dbc0d17ed88c85d2fd13013fafee9e575
Mamun Reza <mamun.reza09@gmail.com>
2024-05-03 01:00:23 +0300

    lc-58 (#10)
    Co-authored-by: Mamun Reza <Muhammad.Reza@energia.ee>

---
+++ lc/csharp/Problems/Lib/Easy/P058.cs
@@ -0,0 +1,22 @@
+﻿namespace Lib.Easy;
+
+public class P058
+{
+    public int LengthOfLastWord(string s)
+    {
+        int length = 0;
+        for (int i = s.Length-1; i >= 0; i--)
+        {
+            if (s[i] != ' ')
+            {
+                length++;
+                continue;
+            }
+
+            if (length > 0)
+                break;
+        }
+
+        return length;
+    }
+}
--- lc/csharp/Problems/Problems.Console/Program.cs
+++ lc/csharp/Problems/Problems.Console/Program.cs
@@ -4,6 +4,6 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        RunnerMedium.ExecuteP053();
+        RunnerEasy.ExecuteP058();
     }
 }
