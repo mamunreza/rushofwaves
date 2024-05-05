7938bc95840cb0da82840f62c9a6af6f31711165
Mamun Reza <mamun.reza09@gmail.com>
2024-05-05 18:22:28 +0300

    lc-290 (#12)
    Co-authored-by: Mamun Reza <Muhammad.Reza@energia.ee>

---
+++ lc/csharp/Problems/Lib/Easy/P290.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+namespace Lib.Easy;
+
+public class P290
+{
+    public bool WordPattern(string pattern, string s)
+    {
+        var words = s.Split(' ');
+
+        if (pattern.Length != words.Length) return false;
+
+        Hashtable c2w = [];
+        Hashtable w2c = [];
+
+        for (int i = 0; i < pattern.Length; i++)
+        {
+            var c = pattern[i];
+            var word = words[i];
+
+            if (c2w.ContainsKey(c) && c2w[c]?.ToString() != word)
+            {
+                return false;
+            }
+            else if(w2c.ContainsKey(word) && (char)w2c[word] != c)
+            {
+                return false;
+            }
+            else
+            {
+                c2w[c] = word;
+                w2c[word] = c;
+            }
+        }
+
+        return true;
+    }
+}
