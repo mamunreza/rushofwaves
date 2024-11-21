be39fc5f95d1a10c35428819dc33098cc63d0593
Mamun Reza <mamun.reza09@gmail.com>
2024-11-21 09:13:13 +0200

    241121001 Basic multicast delegate added (#25)

---
+++ SimpleDelegates/SimpleDelegates/Basics/ConsolePrint.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleDelegates.Basics;
+
+internal class ConsolePrint
+{
+    internal delegate void ConsolePrintDelegate();
+
+    internal static void Method1()
+    {
+        Console.WriteLine("Method 1 invoked.");
+    }
+
+    internal static void Method2()
+    {
+        Console.WriteLine("Method 2 invoked.");
+    }
+}
--- SimpleDelegates/SimpleDelegates/Program.cs
+++ SimpleDelegates/SimpleDelegates/Program.cs
@@ -1,21 +1,9 @@
-﻿using SimpleDelegates.Maths;
-using static SimpleDelegates.Maths.MathWork;
-
-namespace SimpleDelegates;
+﻿namespace SimpleDelegates;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        //Console.WriteLine("Hello, World!");
-
-        MathWork mathWork = new();
-        MathDelegate addDelegate = new(mathWork.Add);
-        MathDelegate subtractDelegate = new(mathWork.Subtract);
-
-        int result = addDelegate(10, 20);
-        Console.WriteLine($"Addition: {result}");
-        int result2 = subtractDelegate(20, 10);
-        Console.WriteLine($"Subtraction: {result2}");
+        ProgramHelpers.ExecuteMultiCastDelegateWork();
     }
 }
