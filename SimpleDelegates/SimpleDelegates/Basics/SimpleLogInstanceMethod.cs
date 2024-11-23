25da0c5ae9f8530c131f0bc79c24a15691f08c40
Mamun Reza <mamun.reza09@gmail.com>
2024-11-23 07:22:20 +0200

    241123001 Delegate log example (#26)

---
+++ SimpleDelegates/SimpleDelegates/Basics/SimpleLogInstanceMethod.cs
@@ -0,0 +1,20 @@
+﻿using static SimpleDelegates.Basics.DelegateContainer;
+
+namespace SimpleDelegates.Basics;
+
+internal class SimpleLogInstanceMethod
+{
+    internal void LogTextToScreen(string message)
+    {
+        Console.WriteLine($"{DateTime.UtcNow} : {message}");
+    }
+    internal void LogTextToFile(string message)
+    {
+        File.AppendAllText("log.txt", $"{DateTime.UtcNow} : {message}\n");
+    }
+
+    internal void LogTextToScreenFromDelegateParameter(LogDelegate logDelegate, string message)
+    {
+        logDelegate(message);
+    }
+}
