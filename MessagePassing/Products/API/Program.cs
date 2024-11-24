4a5d1796595983eccf1d6ed6d40b9998aac62b5f
Mamun Reza <Muhammad.Reza@energia.ee>
2024-11-25 00:15:26 +0200

    MP-2 Changed products api to .net8

--- MessagePassing/Products/API/Program.cs
+++ MessagePassing/Products/API/Program.cs
@@ -1,34 +1,30 @@
 
-namespace API
+namespace API;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+        // Add services to the container.
 
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+        var app = builder.Build();
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
+        app.UseHttpsRedirection();
 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
+        app.UseAuthorization();
 
 
-            app.MapControllers();
+        app.MapControllers();
 
-            app.Run();
-        }
+        app.Run();
     }
 }
