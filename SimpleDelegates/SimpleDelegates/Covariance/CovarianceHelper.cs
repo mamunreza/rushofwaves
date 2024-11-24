786d0f8c403e03c56952e4d358dadd4d6b43798f
Mamun Reza <Muhammad.Reza@energia.ee>
2024-11-24 08:59:07 +0200

    241124001 Covariance example

---
+++ SimpleDelegates/SimpleDelegates/Covariance/CovarianceHelper.cs
@@ -0,0 +1,43 @@
+﻿using static SimpleDelegates.Covariance.DelegateContainer;
+
+namespace SimpleDelegates.Covariance;
+
+public class CovarianceHelper
+{
+    public static void ExecuteCovarianceWork()
+    {
+        FoodFactoryDel breadFactoryDel = FoodFactory.ReturnBread;
+        Food whiteBread = breadFactoryDel(1, "White bread");
+        LogBreadDetailsDel logBreadDetailsDel = LogFoodDetails;
+        logBreadDetailsDel(whiteBread as Bread);
+
+        FoodFactoryDel butterFactoryDel = FoodFactory.ReturnButter;
+        Food unsaltedButter = butterFactoryDel(2, "Unsalted butter");
+        LogButterDetailsDel logButterDetailsDel = LogFoodDetails;
+        logButterDetailsDel(unsaltedButter as Butter);
+
+        Console.ReadKey();
+    }
+
+    internal static void LogFoodDetails(Food food)
+    {
+        if (food is Bread)
+        {
+            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Food.txt"), true))
+            {
+                sw.WriteLine($"Object Type: {food.GetType()}");
+                sw.WriteLine($"Food Details: {food.GetFoodDetails()}");
+            };
+
+        }
+        else if (food is Butter)
+        {
+            Console.WriteLine($"Object Type: {food.GetType()}");
+            Console.WriteLine($"Food Details: {food.GetFoodDetails()}");
+        }
+        else
+        {
+            throw new ArgumentException();
+        }
+    }
+}
---
+++ SimpleDelegates/SimpleDelegates/Covariance/FoodFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleDelegates.Covariance;
+
+public static class FoodFactory
+{
+    public static Bread ReturnBread(int id, string name)
+    {
+        return new Bread { Id = id, Name = name };
+    }
+    public static Butter ReturnButter(int id, string name)
+    {
+        return new Butter { Id = id, Name = name };
+    }
+}
