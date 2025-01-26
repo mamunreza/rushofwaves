6396c178659ab0d13a19399ba6dd9ac3cf79d769
Mamun Reza <Muhammad.Reza@energia.ee>
2025-01-26 23:55:43 +0200

    Implementation of validation/filter using strategy pattern

---
+++ DesignPatterns/DesignPatterns/Strategy/LocationBasedFilter.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.Strategy;
+
+public class LocationBasedFilter : ICustomerSurveyFilter
+{
+    private readonly string _location;
+
+    public LocationBasedFilter(string location)
+    {
+        _location = location;
+    }
+
+    public bool IsSatisfied(CustomerSurvey survey)
+    {
+        return survey.CustomerLocation == _location;
+    }
+}
