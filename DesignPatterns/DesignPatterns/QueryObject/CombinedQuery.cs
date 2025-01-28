00ac6494b1469c69cbfae3364651b491a4a8aa15
Mamun Reza <Muhammad.Reza@energia.ee>
2025-01-28 11:56:02 +0200

    DP-3 QueryObject pattern implemented

---
+++ DesignPatterns/DesignPatterns/QueryObject/CombinedQuery.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPatterns.QueryObject;
+
+public class CombinedQuery : ICustomerSurveyQuery
+{
+    private readonly ICustomerSurveyQuery _query1;
+    private readonly ICustomerSurveyQuery _query2;
+
+    public CombinedQuery(ICustomerSurveyQuery query1, ICustomerSurveyQuery query2)
+    {
+        _query1 = query1;
+        _query2 = query2;
+    }
+
+    public IEnumerable<CustomerSurvey> Execute(IEnumerable<CustomerSurvey> surveys)
+    {
+        return _query1.Execute(surveys).Intersect(_query2.Execute(surveys));
+    }
+}
---
+++ DesignPatterns/DesignPatterns/QueryObject/CustomerSurvey.cs
@@ -0,0 +1,8 @@
+﻿namespace DesignPatterns.QueryObject;
+
+public class CustomerSurvey
+{
+    public int CustomerAge { get; set; }
+    public string CustomerLocation { get; set; }
+    // ... other survey data
+}
