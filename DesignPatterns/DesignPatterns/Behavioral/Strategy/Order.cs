0d3923982483990f96072c73b3b629f2f492ef9c
Mamun Reza <mamun.reza09@gmail.com>
2025-02-07 23:39:18 +0200

    DP-4 Basic strategy pattern (#43)
    Co-authored-by: Mamun Reza <Muhammad.Reza@energia.ee>

---
+++ DesignPatterns/DesignPatterns/Behavioral/Strategy/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatterns.Behavioral.Strategy;
+
+// 3. Context
+public class Order
+{
+    public double Weight { get; set; }
+    public IShippingStrategy ShippingStrategy { get; set; }
+
+    public Order(double weight, IShippingStrategy shippingStrategy)
+    {
+        Weight = weight;
+        ShippingStrategy = shippingStrategy;
+    }
+
+    public double CalculateTotalShippingCost()
+    {
+        return ShippingStrategy.CalculateCost(this);
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Behavioral/Strategy/OvernightShipping.cs
@@ -0,0 +1,9 @@
+﻿namespace DesignPatterns.Behavioral.Strategy;
+
+public class OvernightShipping : IShippingStrategy
+{
+    public double CalculateCost(Order order)
+    {
+        return 20.0 + order.Weight * 0.5; // Example calculation
+    }
+}
