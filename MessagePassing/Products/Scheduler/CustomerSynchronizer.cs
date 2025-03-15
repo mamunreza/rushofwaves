5ee0c723bf8084185392a14c13997a681864df86
Mamun Reza <Muhammad.Reza@energia.ee>
2025-03-16 01:35:13 +0200

    Sync customer data from inbox

--- MessagePassing/Products/Scheduler/CustomerSynchronizer.cs
+++ MessagePassing/Products/Scheduler/CustomerSynchronizer.cs
@@ -5,7 +5,7 @@ namespace MessagePassing.Products.Scheduler;
 
 internal interface ICustomerSynchronizer
 {
-    Task SynchronizeCustomerAddedAsync(CancellationToken cancellationToken);
+    public Task SynchronizeCustomerAddedAsync(CancellationToken cancellationToken);
 }
 
 internal class CustomerSynchronizer : ICustomerSynchronizer
@@ -27,24 +27,34 @@ internal class CustomerSynchronizer : ICustomerSynchronizer
         _logger.LogInformation("Synchronization started for customer added");
 
         var inboxItems = await _appDbContext.CustomerAddedInboxes.Where(x => !x.IsProcessed).Take(100).ToListAsync(cancellationToken);
+        var processedCustomerIds = await _appDbContext.Customers
+            .Where(c => inboxItems.Select(x => x.RootId).Contains(c.Id))
+            .Select(c => c.Id)
+            .ToListAsync(cancellationToken);
+
         foreach (var inboxItem in inboxItems)
         {
-            // Synchronize customer added
-            var customer = new Customer
+            if (processedCustomerIds.Contains(inboxItem.RootId))
             {
-                Id = inboxItem.RootId,
-                FirstName = inboxItem.FirstName,
-                LastName = inboxItem.LastName,
-                Email = inboxItem.Email,
-                Phone = inboxItem.Phone,
-                CreatedAt = inboxItem.CreatedAt,
-                UpdatedAt = inboxItem.UpdatedAt
-            };
+                _logger.LogInformation("Customer already processed, id: {CustomerId}", inboxItem.RootId);
+            }
+            else
+            {
+                var customer = new Customer
+                {
+                    Id = inboxItem.RootId,
+                    FirstName = inboxItem.FirstName,
+                    LastName = inboxItem.LastName,
+                    Email = inboxItem.Email,
+                    Phone = inboxItem.Phone,
+                    CreatedAt = inboxItem.CreatedAt,
+                    UpdatedAt = inboxItem.UpdatedAt
+                };
+                _appDbContext.Customers.Add(customer);
+            }
             inboxItem.IsProcessed = true;
-            _appDbContext.Customers.Add(customer);
+            await _appDbContext.SaveChangesAsync(cancellationToken);
         }
-        await _appDbContext.SaveChangesAsync(cancellationToken);
-
         _logger.LogInformation("Synchronization ended for customer added");
     }
 }
