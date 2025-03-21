f5782f22d103eb29395733646e068f3c45a443d4
Mamun Reza <mamun.reza09@gmail.com>
2025-03-22 00:59:47 +0200

    ProdocutAddedCommand command introduced (#69)

---
+++ MessagePassing/Products/API/Domain/ProdocutAddedCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace API.Domain;
+
+public class ProdocutAddedCommand
+{
+    public required string Name { get; set; }
+    public decimal Price { get; set; }
+    public string? Description { get; set; }
+    public ProductCategory ProductCategory { get; set; }
+    public string? ImageUrl { get; set; }
+}
+
+public enum ProductCategory
+{
+    Electronics,
+    Clothing,
+    Home,
+    Garden,
+    Beauty,
+    Toys,
+    Food,
+    Other
+}
